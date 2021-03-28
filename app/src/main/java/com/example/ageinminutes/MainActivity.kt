3408ea05a3fe479438b02530d6e0b95e39dde85e
Viktor Stiblo <viktor.st.spb@gmail.com>
2021-03-28 19:42:06 +0300

    First commit - completed UI

---
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.ageinminutes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.ButtonBarLayout
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
