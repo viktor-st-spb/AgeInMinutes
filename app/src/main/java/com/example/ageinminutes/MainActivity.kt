50eb57548248f3315e44c0dc1292830282c91f3c
viktor-ds <viktor-ds@yandex.ru>
2021-03-30 14:17:35 +0300

    Created button on click listener

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.ageinminutes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnDatePicker.setOnClickListener {
+            Toast.makeText(this, "OK", Toast.LENGTH_LONG).show() }
 
     }
 }
