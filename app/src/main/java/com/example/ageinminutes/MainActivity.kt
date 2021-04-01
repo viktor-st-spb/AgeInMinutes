b777db5419a9537cc15412e9a2c3bbb2f4ec1a93
viktor-ds <viktor-ds@yandex.ru>
2021-04-01 13:57:11 +0300

    Added DatePicker OnClickListener functionality

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -38,17 +39,33 @@ class MainActivity : AppCompatActivity() {
 
         DatePickerDialog(this,
                 DatePickerDialog.OnDateSetListener {
-                    view, SelectedYear, SelectedMonth, SelectedDayOfMonth ->
+                    view, selectedYear, selectedMonth, selectedDayOfMonth ->
 
                     //TODO - this code will be executed after the date will be selected
                     Toast.makeText(this,
-                            "The year is $SelectedYear, the month is  " +
-                                    "and the date is $SelectedDayOfMonth" ,
+                            "The year is $selectedYear, the month is  " +
+                                    "and the date is $selectedDayOfMonth" ,
                             Toast.LENGTH_LONG).show()
 
-                    val selectedDate = "$SelectedDayOfMonth/${SelectedMonth+1}/$SelectedYear"
+                    val selectedDate = "$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
 
                     tvSelectedDate.setText(selectedDate)
+
+                    val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                    val theDate = sdf.parse(selectedDate)
+
+                    val selectedDateInMinutes = theDate!!.time / 60000
+                    val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+
+                    val currentDateInMinutes = currentDate!!.time / 60000
+
+                    val differenceInMinutes = currentDateInMinutes - selectedDateInMinutes
+
+                    tvSelectedDateInMinutes.text = differenceInMinutes.toString()
+
+
+
+
 
                 }, year, month, day)
                 .show()
